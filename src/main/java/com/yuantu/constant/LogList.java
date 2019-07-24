df962a0a44f9800740c7d4e72b50f18658587b7d
Crispy-Rice <786879138@qq.com>
2019-07-24 18:54:42 +0800

    v7.24

---
+++ src/main/java/com/yuantu/constant/LogList.java
@@ -0,0 +1,11 @@
+package com.yuantu.constant;
+
+/**
+ * @ClassName LogList
+ * @Description TODO
+ * @Author yukun
+ * @Date 2019/7/24 15:46
+ * @Version 1.0
+ **/
+public class LogList {
+}
---
+++ src/main/java/com/yuantu/util/LogUtil.java
@@ -0,0 +1,11 @@
+package com.yuantu.util;
+
+/**
+ * @ClassName LogUtil
+ * @Description TODO
+ * @Author yukun
+ * @Date 2019/7/24 15:40
+ * @Version 1.0
+ **/
+public class LogUtil {
+}
